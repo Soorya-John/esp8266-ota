0a700863b51d0616659646029721e5769e51b9b0
Soorya_ <86581603+5i344A@users.noreply.github.com>
2025-06-21 03:54:42 +0530

    Update ani_esp.ino to v6.4

--- working code for esp8266 d1 mini/ani_esp.ino
+++ working code for esp8266 d1 mini/ani_esp.ino
@@ -1,171 +1,335 @@
-// FULL POLISHED 10-IN-1 RETRO GAME CONSOLE (SH1106 Version using U8G2)
-#include <Arduino.h>
-#include <U8g2lib.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+#define OLED_RESET -1
 
-// Use appropriate constructor for your ESP32 and SH1106 wiring
-U8G2_SH1106_128X64_NONAME_F_HW_I2C display(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+void vibrateBuzz(int duration);
 
-#define BTN_UP    32
-#define BTN_DOWN  33
-#define BTN_LEFT  25
-#define BTN_RIGHT 26
-#define BTN_A     27
-#define BTN_B     14
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-const char* games[] = {
-  "1. Snake",
-  "2. Flappy",
-  "3. Dino Run",
-  "4. Pong",
-  "5. Breakout",
-  "6. Dodge",
-  "7. Bounce",
-  "8. Catch",
-  "9. Shoot",
-  "10. Jump"
+const int buttonA = D3;
+const int buttonB = D4;
+const int buttonOTA = D1;
+
+const char* ssidList[] = {"Soorya ", "OnePlus12", "LoveLink"};
+const char* passwordList[] = {"mgm123@#", "gfg123@#", "heartwifi@2024"};
+const char* pushToken = "o.ECclJ7uRP0wqiXXn0ryZflwgPS9lk9nJ";
+const char* host = "api.pushbullet.com";
+const int httpsPort = 443;
+
+const char* otaUrl = "https://esp8266-ota.pages.dev/backup.bin";
+
+const char* memories[] = {
+  "1st & foremost I love you",
+  "14th Nov - Made you mine\n6 is your bday, I remember ok hehe",
+  "Those little dates we went together\nwere the best days of my life ",
+  "When you made me feel safe,\nI really felt like a kid",
+  "Holding your hands is still my weakness",
+  "ur cheeks r to die for",
+  "My kid is damn lucky to have u",
+  "never ever feel like u r not enough\nbecause u are",
+  "when things get tough, call me\n ill never let u go",
+  "no matter how tough it gets ill always be there",
+  "u really annoy me but i love it",
+  "u bring the spice into my life",
+  "u are gonnna be a great doctor",
+  "ur belly is really cozy to hold",
+  "u r perfect baby",
+  "remember the time u made me eat\nall the left over chocolala on feb 14th?",
+  "wear the ring idiot ik u not wearing rn"
 };
-const int gameCount = sizeof(games)/sizeof(games[0]);
-int selectedGame = 0;
-bool inGame = false;
+const int memoryCount = sizeof(memories) / sizeof(memories[0]);
+int currentMemory = 0;
+unsigned long lastMemoryTime = 0;
 
-int highScores[10] = {0};
+enum State { SPLASH, HEART, MEMORY, SENDING, SENT_OK, SENT_FAIL };
+State screenState = SPLASH;
+unsigned long screenTimer = 0;
+bool heartBeatUp = true;
+unsigned long lastHeartUpdate = 0;
 
-bool isPressed(int pin) { return digitalRead(pin) == LOW; }
-void waitRelease(int pin) { while (isPressed(pin)) delay(10); }
+unsigned long lastDebounceTimeA = 0;
+unsigned long lastDebounceTimeB = 0;
+unsigned long lastDebounceTimeOTA = 0;
+const unsigned long debounceDelay = 50;
 
-void animatedSplash() {
-  display.clearBuffer();
-  for (int i = 0; i < SCREEN_WIDTH; i += 4) {
-    display.drawLine(0, 0, i, SCREEN_HEIGHT);
-    display.drawLine(SCREEN_WIDTH - i, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    display.sendBuffer(); delay(15);
-  }
-  display.setFont(u8g2_font_ncenB08_tr);
-  display.drawStr(24, 32, "MiniBox");
-  display.setFont(u8g2_font_6x10_tr);
-  display.drawStr(10, 50, "10-in-1 Retro Games!");
-  display.sendBuffer(); delay(2000);
-}
-
-void drawMenu() {
-  display.clearBuffer();
-  display.setFont(u8g2_font_6x10_tr);
-  display.drawStr(24, 10, "== Game Menu ==");
-  int start = selectedGame > 4 ? selectedGame - 4 : 0;
-  int end = min(start + 8, gameCount);
-  for (int i = start; i < end; i++) {
-    int y = 20 + (i - start) * 8;
-    if (i == selectedGame) display.drawStr(0, y, ">");
-    display.setCursor(10, y); display.print(games[i]);
-    char scoreStr[5]; sprintf(scoreStr, "%d", highScores[i]);
-    display.setCursor(100, y); display.print(scoreStr);
-  }
-  display.sendBuffer();
-}
-
-void gameOverScreen(const char* gameName, int score, int gameIndex) {
-  if (score > highScores[gameIndex]) highScores[gameIndex] = score;
-  display.clearBuffer();
-  display.setCursor(0, 10); display.print("Game Over: "); display.println(gameName);
-  display.setCursor(0, 20); display.print("Score: "); display.println(score);
-  display.setCursor(0, 30); display.print("Highscore: "); display.println(highScores[gameIndex]);
-  display.sendBuffer(); delay(2000);
-  waitRelease(BTN_A); waitRelease(BTN_B);
-}
-
-// === GAMES ===
-void playSnake() {
-  const int cellSize = 4;
-  const int cols = 128 / cellSize;
-  const int rows = 64 / cellSize;
-
-  struct Point { int x, y; };
-  Point snake[128];
-  int length = 3;
-  int dir = 0; // 0-right, 1-down, 2-left, 3-up
-  Point food = { random(0, cols), random(0, rows) };
-
-  for (int i = 0; i < length; i++) snake[i] = {cols/2 - i, rows/2};
-
-  unsigned long lastMove = 0;
-
-  while (true) {
-    if (isPressed(BTN_LEFT) && dir != 0) { dir = 2; waitRelease(BTN_LEFT); }
-    if (isPressed(BTN_RIGHT) && dir != 2) { dir = 0; waitRelease(BTN_RIGHT); }
-    if (isPressed(BTN_UP) && dir != 1) { dir = 3; waitRelease(BTN_UP); }
-    if (isPressed(BTN_DOWN) && dir != 3) { dir = 1; waitRelease(BTN_DOWN); }
-
-    if (millis() - lastMove < 150) continue;
-    lastMove = millis();
-
-    for (int i = length; i > 0; i--) snake[i] = snake[i - 1];
-    if (dir == 0) snake[0].x++;
-    else if (dir == 1) snake[0].y++;
-    else if (dir == 2) snake[0].x--;
-    else if (dir == 3) snake[0].y--;
-
-    if (snake[0].x == food.x && snake[0].y == food.y) {
-      length++;
-      food = { random(0, cols), random(0, rows) };
-    }
-
-    if (snake[0].x < 0 || snake[0].x >= cols || snake[0].y < 0 || snake[0].y >= rows) break;
-    for (int i = 1; i < length; i++) {
-      if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) break;
-    }
-
-    display.clearBuffer();
-    for (int i = 0; i < length; i++)
-      display.drawBox(snake[i].x * cellSize, snake[i].y * cellSize, cellSize, cellSize);
-    display.drawFrame(food.x * cellSize, food.y * cellSize, cellSize, cellSize);
-    display.sendBuffer();
-  }
-  gameOverScreen("Snake", length - 3, 0);
-}
-
-void playFlappy() { gameOverScreen("Flappy", random(10, 50), 1); }
-void playDinoRun() { gameOverScreen("Dino", random(10, 50), 2); }
-void playPong() { gameOverScreen("Pong", random(10, 50), 3); }
-void playBreakout() { gameOverScreen("Breakout", random(10, 50), 4); }
-void playDodge() { gameOverScreen("Dodge", random(10, 50), 5); }
-void playBounce() { gameOverScreen("Bounce", random(10, 50), 6); }
-void playCatch() { gameOverScreen("Catch", random(10, 50), 7); }
-void playShoot() { gameOverScreen("Shoot", random(10, 50), 8); }
-void playJump() { gameOverScreen("Jump", random(10, 50), 9); }
+const int motorPin = D0;
 
 void setup() {
-  for (int pin : {BTN_UP, BTN_DOWN, BTN_LEFT, BTN_RIGHT, BTN_A, BTN_B}) pinMode(pin, INPUT_PULLUP);
-  display.begin();
-  display.clearBuffer(); display.sendBuffer();
-  animatedSplash(); drawMenu();
+  Serial.begin(115200);
+  pinMode(buttonA, INPUT_PULLUP);
+  pinMode(buttonB, INPUT_PULLUP);
+  pinMode(buttonOTA, INPUT_PULLUP);
+  pinMode(motorPin, OUTPUT);
+  digitalWrite(motorPin, LOW);
+
+  vibrateBuzz(150);
+  delay(100);
+  vibrateBuzz(150);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+
+  showSplash();
+  delay(2500);
+  showHeart();
+  screenState = HEART;
 }
 
 void loop() {
-  static int lastSelection = -1;
-  static unsigned long lastInputTime = 0;
-  if (!inGame) {
-    if (millis() - lastInputTime > 150) {
-      if (isPressed(BTN_DOWN)) { selectedGame = (selectedGame + 1) % gameCount; waitRelease(BTN_DOWN); lastInputTime = millis(); }
-      if (isPressed(BTN_UP)) { selectedGame = (selectedGame - 1 + gameCount) % gameCount; waitRelease(BTN_UP); lastInputTime = millis(); }
-      if (isPressed(BTN_A)) { inGame = true; lastSelection = -1; waitRelease(BTN_A); lastInputTime = millis(); }
-    }
-    if (selectedGame != lastSelection) { drawMenu(); lastSelection = selectedGame; }
-  } else {
-    switch (selectedGame) {
-      case 0: playSnake(); break;
-      case 1: playFlappy(); break;
-      case 2: playDinoRun(); break;
-      case 3: playPong(); break;
-      case 4: playBreakout(); break;
-      case 5: playDodge(); break;
-      case 6: playBounce(); break;
-      case 7: playCatch(); break;
-      case 8: playShoot(); break;
-      case 9: playJump(); break;
-    }
-    delay(200); inGame = false; drawMenu();
+  static bool lastA = HIGH;
+  static bool lastB = HIGH;
+  static bool lastOTA = HIGH;
+
+  bool currA = digitalRead(buttonA);
+  bool currB = digitalRead(buttonB);
+  bool currOTA = digitalRead(buttonOTA);
+
+  if (lastA == HIGH && currA == LOW && (millis() - lastDebounceTimeA) > debounceDelay) {
+    lastDebounceTimeA = millis();
+    handleButtonA();
   }
+  if (lastB == HIGH && currB == LOW && (millis() - lastDebounceTimeB) > debounceDelay) {
+    lastDebounceTimeB = millis();
+    handleButtonB();
+  }
+  if (lastOTA == HIGH && currOTA == LOW && (millis() - lastDebounceTimeOTA) > debounceDelay) {
+    lastDebounceTimeOTA = millis();
+    handleOTAUpdate();
+  }
+
+  lastA = currA;
+  lastB = currB;
+  lastOTA = currOTA;
+
+  if (screenState == MEMORY && millis() - lastMemoryTime > 45000) {
+    showHeart();
+    screenState = HEART;
+  }
+
+  if (screenState == HEART && millis() - lastHeartUpdate > 500) {
+    drawBeatingHeart(heartBeatUp);
+    heartBeatUp = !heartBeatUp;
+    lastHeartUpdate = millis();
+  }
+
+  if ((screenState == SENT_OK || screenState == SENT_FAIL) && millis() > screenTimer) {
+    showHeart();
+    screenState = HEART;
+  }
+}
+
+void handleButtonA() {
+  vibrateBuzz(120);
+  currentMemory = (currentMemory + 1) % memoryCount;
+  display.clearDisplay();
+  display.drawRoundRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 1, 5, SSD1306_WHITE);
+  display.setTextSize(1);
+  display.setCursor(15, 3);
+  display.println("Our Memory Vault");
+  display.drawFastHLine(10, 13, 108, SSD1306_WHITE);
+  display.setCursor(5, 20);
+  display.setTextWrap(true);
+  display.println(memories[currentMemory]);
+  display.display();
+  screenState = MEMORY;
+  lastMemoryTime = millis();
+}
+
+void handleButtonB() {
+  vibrateBuzz(120);
+  sendPushNotification();
+}
+
+void showSplash() {
+  display.clearDisplay();
+  display.drawRoundRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 1, 6, SSD1306_WHITE);
+  display.setTextSize(2);
+  display.setCursor(20, 22);
+  display.println("For Ani");
+  display.setTextSize(1);
+  display.setCursor(28, 46);
+  display.println("With Love <3");
+  display.display();
+}
+
+void showHeart() {
+  screenState = HEART;
+  lastHeartUpdate = 0;
+  heartBeatUp = true;
+  drawBeatingHeart(true);
+}
+
+void drawBeatingHeart(bool large) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(36, 0);
+  display.println("Love u ani");
+
+  int cx = SCREEN_WIDTH / 2;
+  int cy = SCREEN_HEIGHT / 2 + 10;
+  int r = large ? 8 : 6;
+
+  display.fillCircle(cx - r, cy - r, r, SSD1306_WHITE);
+  display.fillCircle(cx + r, cy - r, r, SSD1306_WHITE);
+  display.fillTriangle(cx - 2 * r, cy - r, cx + 2 * r, cy - r, cx, cy + 2 * r, SSD1306_WHITE);
+
+  for (int i = 0; i < 10; i++) {
+    int x = random(0, SCREEN_WIDTH);
+    int y = random(0, SCREEN_HEIGHT);
+    if (random(0, 2) == 0) {
+      display.drawPixel(x, y, SSD1306_WHITE);
+    }
+  }
+
+  display.display();
+}
+
+void showMessage(const char* msg) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.drawRoundRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 1, 5, SSD1306_WHITE);
+  display.setCursor(10, 28);
+  display.print(msg);
+  display.display();
+}
+
+void sendPushNotification() {
+  screenState = SENDING;
+  showMessage("Sending magic...");
+
+  if (WiFi.status() != WL_CONNECTED) {
+    for (int i = 0; i < 3; i++) {
+      WiFi.begin(ssidList[i], passwordList[i]);
+      unsigned long start = millis();
+      while (WiFi.status() != WL_CONNECTED && millis() - start < 5000) {
+        delay(200);
+      }
+      if (WiFi.status() == WL_CONNECTED) break;
+    }
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    showMessage("Wi-Fi Failed");
+    screenState = SENT_FAIL;
+    screenTimer = millis() + 3000;
+    return;
+  }
+
+  WiFiClientSecure client;
+  client.setInsecure();
+
+  if (!client.connect(host, httpsPort)) {
+    showMessage("Push Failed");
+    screenState = SENT_FAIL;
+    screenTimer = millis() + 3000;
+    return;
+  }
+
+  String body = "{\"type\": \"note\", \"title\": \"From Ani\", \"body\": \"She pressed the button!\"}";
+
+  client.print(String("POST ") + "/v2/pushes" + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Authorization: Bearer " + pushToken + "\r\n" +
+               "Content-Type: application/json\r\n" +
+               "Content-Length: " + body.length() + "\r\n\r\n" +
+               body);
+
+  showMessage("Delivered with love");
+  screenState = SENT_OK;
+  screenTimer = millis() + 3000;
+}
+
+void vibrateBuzz(int duration = 150) {
+  digitalWrite(motorPin, HIGH);
+  delay(duration);
+  digitalWrite(motorPin, LOW);
+}
+
+void handleOTAUpdate() {
+  for (int i = 3; i > 0; i--) {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(30, 5);
+    display.println("OTA Update v6.4");
+    display.setTextSize(2);
+    display.setCursor(10, 25);
+    display.println("Starting in");
+    display.setCursor(50, 48);
+    display.print(i);
+    display.display();
+    delay(1000);
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setCursor(30, 22);
+  display.println("Checking for");
+  display.setCursor(40, 35);
+  display.println("updates");
+  display.display();
+
+  display.setCursor(45, 40);
+  display.println("2...");
+  display.display();
+  delay(1000);
+
+  display.setCursor(45, 40);
+  display.println("1...");
+  display.display();
+  delay(1000);
+
+  showMessage("Checking for updates...");
+
+  if (WiFi.status() != WL_CONNECTED) {
+    for (int i = 0; i < 3; i++) {
+      WiFi.begin(ssidList[i], passwordList[i]);
+      unsigned long start = millis();
+      while (WiFi.status() != WL_CONNECTED && millis() - start < 5000) {
+        delay(200);
+      }
+      if (WiFi.status() == WL_CONNECTED) break;
+    }
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    showMessage("Wi-Fi Failed");
+    delay(3000);
+    showHeart();
+    return;
+  }
+
+  WiFiClientSecure client;
+  client.setInsecure();
+
+  Serial.print("Connecting to: ");
+  Serial.println(otaUrl);
+
+  t_httpUpdate_return ret = ESPhttpUpdate.update(client, otaUrl);
+
+  Serial.printf("OTA Result: %d - %s\n", ret, ESPhttpUpdate.getLastErrorString().c_str());
+
+  switch (ret) {
+    case HTTP_UPDATE_FAILED:
+      showMessage("OTA Failed");
+      break;
+    case HTTP_UPDATE_NO_UPDATES:
+      showMessage("No Update Found");
+      break;
+    case HTTP_UPDATE_OK:
+      showMessage("Updated! Rebooting");
+      delay(1000);
+      ESP.restart();
+      break;
+  }
+
+  delay(3000);
+  showHeart();
 }
